b755b1f54db898fd38361932950c802c886a0d36
AliciaAMT <46018401+AliciaAMT@users.noreply.github.com>
2025-08-14 05:26:56 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dcci-website',
+  webDir: 'www'
+};
+
+export default config;
