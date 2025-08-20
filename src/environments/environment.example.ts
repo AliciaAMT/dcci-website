2d486ea8ad252ff380f7818bbe10b5b46b34d0fa
AliciaAMT <46018401+AliciaAMT@users.noreply.github.com>
2025-08-20 17:24:24 -0500

    chore: track environment.example.ts files

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "__PUBLIC_WEB_API_KEY__",
+    authDomain: "__PROJECT__.firebaseapp.com",
+    projectId: "__PROJECT__",
+    storageBucket: "__PROJECT__.appspot.com",
+    messagingSenderId: "__SENDER_ID__",
+    appId: "__APP_ID__"
+  },
+  disqusShortname: "__DISQUS_SHORTNAME__"
+};
