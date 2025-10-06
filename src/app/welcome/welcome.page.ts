9d68545dd8bac4eb2c43cd9e3696aa9f6e89106b
AliciaAMT <46018401+AliciaAMT@users.noreply.github.com>
2025-10-05 21:01:02 -0500

    feat: add welcome route to application for improved user navigation

---
+++ src/app/welcome/welcome.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {
+  IonContent,
+  IonIcon
+} from '@ionic/angular/standalone';
+import { VersionService } from '../services/version.service';
+import { ContactFormComponent } from '../components/contact-form.component';
+import { NewsletterSignupComponent } from '../components/newsletter-signup.component';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.page.html',
+  styleUrls: ['./welcome.page.scss'],
+  imports: [IonContent, IonIcon, ContactFormComponent, NewsletterSignupComponent],
+  standalone: true
+})
+export class WelcomePage {
+  version: string;
+
+  constructor(private versionService: VersionService) {
+    this.version = this.versionService.getVersion();
+  }
+}
