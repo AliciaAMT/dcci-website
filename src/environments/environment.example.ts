6b6059db93e4171b424737fd32affc4b1288a742
Alicia A Taylor <aliciataylorguitar@gmail.com>
2026-01-10 11:38:08 -0600

    chore: update version numbers to 1.0.1 in package.json and environment.example.ts; refresh cache for Firebase hosting assets

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  version: '1.0.0',
+  version: '1.0.1',
   firebase: {
     apiKey: "__PUBLIC_WEB_API_KEY__",
     authDomain: "__PROJECT__.firebaseapp.com",
